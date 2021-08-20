ef8551cbc20b61dff4fd1bdd81d76a95bd336f8a
Djaysel Pessôa <djaysel@outlook.com>
2021-08-20 15:58:23 -0300

    Create abrirBrowser.py

---
+++ pacoteEscolha/abrirBrowser.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+
+def abrirBrowser():
+    webbrowser.open("https://stackoverflow.com/questions/22125114/how-to-insert-links-in-python")
+    webbrowser.open("https://isolution.pro/pt/t/python/membership-operators-example/exemplo-de-operadores-de-associacao-python") 
+    webbrowser.open("https://realpython.com/python-is-identity-vs-equality/")
+    webbrowser.open("https://acervolima.com/python-3-operadores-logicos/")
+    webbrowser.open("http://excript.com/python/operador-de-atribuicao-python.html")
+    webbrowser.open("http://cristiancechinel.pro.br/my_files/algorithms/bookhtml/node39.html")
+    webbrowser.open("https://isolution.pro/pt/t/python3/python-basic-operators/python-3-operadores-basicos")
+    webbrowser.open("https://www.devmedia.com.br/operadores-no-python/40693")
+    return ""
