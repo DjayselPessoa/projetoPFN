299194c3efe8e36f12758b7108768d43ee74827a
Djaysel Pessôa <djaysel@outlook.com>
2021-09-04 17:33:25 -0300

    Update inicio..py

--- inicio..py
+++ inicio..py
@@ -1,18 +1,18 @@
-from pacoteDef.defEscolha import escolha
+from PacoteDef.DefEscolha import ObjEscolha
 
 
 ativo = True
 sair = ""
-print("\n----PYTHON FOR NOOBS----\n")
+print("\n---- PYTHON FOR NOOBS ----\n")
 
 while ativo:
     try:
         escolhaOp = ""
-        print("\n-----------------------------------------------------------------------------PFN-------DjPESSÔA----")
+        print("\n", "-" * 76, "PFN-------DjPESSÔA -")
 
         print("\nAbaixo estão todos os tipos de operadores:\n","\n1 - Operadores Aritméticos\n2 - Operadores de Atribuição\n3 - Operadores de Comparação\n4 - Operadores Lógicos\n5 - Operadores de Identidade\n6 - Operadores de Associação\n7 - Sobre o programa\n8 - Sair\n","\n")
         escolhaOp = str(input("Escolha qual destes tipos de operadores você deseja estudar: "))
-        sair = escolha(escolhaOp)
+        sair = ObjEscolha.escolha(escolhaOp)
         if sair == "ok":
             break
 
