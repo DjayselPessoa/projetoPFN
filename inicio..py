7fca244e14ea5fd4b896ea7d422fa62250642c67
Djaysel Pessôa <djaysel@outlook.com>
2021-09-06 21:25:40 -0300

    Update inicio..py

--- inicio..py
+++ inicio..py
@@ -1,20 +1,32 @@
 from PacoteDef.DefEscolha import ObjEscolha
 
 
-ativo = True
-sair = ""
-print("\n---- PYTHON FOR NOOBS ----\n")
+class Inicio:
+    def inicio(self, nome, score, active):
+        self.nome = nome
+        nome = self.nome
+        self.score = score
+        score = self.score
+        self.active = active
+        active = self.active
 
-while ativo:
-    try:
-        escolhaOp = ""
-        print("\n", "-" * 76, "PFN-------DjPESSÔA -")
+        sair = ""
+        print("\n---- PYTHON FOR NOOBS ----\n")
 
-        print("\nAbaixo estão todos os tipos de operadores:\n","\n1 - Operadores Aritméticos\n2 - Operadores de Atribuição\n3 - Operadores de Comparação\n4 - Operadores Lógicos\n5 - Operadores de Identidade\n6 - Operadores de Associação\n7 - Sobre o programa\n8 - Sair\n","\n")
-        escolhaOp = str(input("Escolha qual destes tipos de operadores você deseja estudar: "))
-        sair = ObjEscolha.escolha(escolhaOp)
-        if sair == "ok":
-            break
+        while active:
+            try:
+                escolhaOp = ""
+                print("\n", "-" * 76, "PFN-------DjPESSÔA -")
+                print(f"User: {nome} <-> SCORE: {score}")
+                print("\nAbaixo estão todos os tipos de operadores:\n","\n1 - Operadores Aritméticos\n2 - Operadores de Atribuição\n3 - Operadores de Comparação\n4 - Operadores Lógicos\n5 - Operadores de Identidade\n6 - Operadores de Associação\n7 - Sobre o programa\n8 - Sair\n","\n")
+                escolhaOp = str(input("Escolha qual destes tipos de operadores você deseja estudar: "))
+                sair = ObjEscolha.escolha(escolhaOp)
+                if sair == "ok":
+                    active = False
+                    return active
 
-    except ValueError as e:
-        print("Erro", e)
+            except ValueError as e:
+                print("Erro", e)
+
+
+ObjInicio = Inicio()
