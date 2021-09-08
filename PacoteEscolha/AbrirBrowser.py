aa3093815d823019cce1cd951b2c82902259224a
Djaysel Pessôa <djaysel@outlook.com>
2021-09-08 06:27:41 -0300

    Update AbrirBrowser.py

--- PacoteEscolha/AbrirBrowser.py
+++ PacoteEscolha/AbrirBrowser.py
@@ -12,6 +12,15 @@ class AbrirBrowser:
         webbrowser.open("http://cristiancechinel.pro.br/my_files/algorithms/bookhtml/node39.html")
         webbrowser.open("https://isolution.pro/pt/t/python3/python-basic-operators/python-3-operadores-basicos")
         webbrowser.open("https://www.devmedia.com.br/operadores-no-python/40693")
+        webbrowser.open("https://pythonspot.com/files-spreadsheets-csv/")
+        webbrowser.open("https://stackoverflow.com/questions/41039058/create-a-new-csv-file")
+        webbrowser.open("https://stackoverflow.com/questions/40476677/specify-newline-character-n-in-reading-csv-using-python")
+        webbrowser.open("https://thispointer.com/python-how-to-append-a-new-row-to-an-existing-csv-file/")
+        webbrowser.open("https://stackoverflow.com/questions/1466000/difference-between-modes-a-a-w-w-and-r-in-built-in-open-function")
+        webbrowser.open("https://www.programiz.com/python-programming/")
+        webbrowser.open("https://stackoverflow.com/questions/1526607/extracting-data-from-a-csv-file-in-python")
+        webbrowser.open("https://www.geeksforgeeks.org/visualize-data-from-csv-file-in-python/")
+        webbrowser.open("https://desenvolvimentoaberto.org/2014/02/21/io-open-read-write-seek-python-linux/")
         return
 
 
