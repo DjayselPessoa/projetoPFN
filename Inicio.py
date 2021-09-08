42302fd1b3cc1655dd52ca1825234d1fe86266e6
Djaysel Pessôa <djaysel@outlook.com>
2021-09-08 04:25:18 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -2,31 +2,38 @@ from PacoteDef.DefEscolha import ObjEscolha
 
 
 class Inicio:
-    def inicio(self, nome, score, active):
+    def inicio(self, nome, score, cod, active):
+        self.cod = cod
         self.nome = nome
-        nome = self.nome
         self.score = score
-        score = self.score
         self.active = active
-        active = self.active
 
-        sair = ""
-        print("\n---- PYTHON FOR NOOBS ----\n")
+        sair = "no"
+        escolhaOp = str
 
         while active:
             try:
-                escolhaOp = ""
-                print("\n", "-" * 76, "PFN-------DjPESSÔA -")
-                print(f"User: {nome} <-> SCORE: {score}")
-                print("\nAbaixo estão todos os tipos de operadores:\n","\n1 - Operadores Aritméticos\n2 - Operadores de Atribuição\n3 - Operadores de Comparação\n4 - Operadores Lógicos\n5 - Operadores de Identidade\n6 - Operadores de Associação\n7 - Sobre o programa\n8 - Sair\n","\n")
-                escolhaOp = str(input("Escolha qual destes tipos de operadores você deseja estudar: "))
-                sair = ObjEscolha.escolha(escolhaOp)
+                escolhaOp = str
+
+                print(" " * 15,"---- PYTHON FOR NOOBS ----\n")
+                print("\n", "-" * 76, "PFN-------DjPESSÔA -")             
+                print(f"User: {self.nome} <-> SCORE: {self.score}")
+                print("\nAbaixo estão todos os tipos de operadores:\n\n1 - Operadores Aritméticos\n2 - Operadores de Atribuição\n3 - Operadores de Comparação\n4 - Operadores Lógicos\n5 - Operadores de Identidade\n6 - Operadores de Associação\n7 - Sobre o programa\n8 - Sair\n","\n")
+                escolhaOp = str(input("Escolha os operadores que deseja estudar: "))
+                sair, self.score, codFinal = ObjEscolha.escolha(escolhaOp, self.score, self.cod)
+                # print("scoreFinal", self.score)
+
                 if sair == "ok":
-                    active = False
-                    return active
+                    self.active = False
+                    # print("scoreInicio", self.score)
+                    return self.active, self.score, codFinal
+                elif sair == "no":
+                    self.active = True
+                else:
+                    raise ValueError("ERRO DESCONHECIDO!")
 
             except ValueError as e:
-                print("Erro", e)
+                print("LOG: -> ", e)
 
 
 ObjInicio = Inicio()
