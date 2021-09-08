5ceae31d0b8c67fc9628b7fa2a8ba74503fea512
Djaysel Pessôa <djaysel@outlook.com>
2021-09-08 17:29:55 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -1,39 +1,85 @@
-from PacoteDef.DefEscolha import ObjEscolha
+from Core.AbrirCSV import ObjAbrirSCORE
+from LayerMain import ObjLayerMain
+from Core.Entrar import ObjEntrar
+from Core.AbrirCSV import ObjAbrirCSV
+from Data.CriarProfile import ObjCriarProfile
+from Data.UpdateScore import ObjUpdateScore
+from csv import writer
+from csv import QUOTE_MINIMAL
 
+active = True
 
-class Inicio:
-    def inicio(self, nome, score, cod, active):
-        self.cod = cod
-        self.nome = nome
-        self.score = score
-        self.active = active
+while active:
+    try:
+        alert = 0
+        cod = []
+        nome = []
+        senha = []
+        scorename = []
 
-        sair = "no"
-        escolhaOp = str
+        alert, cod, nome, senha, scorename = ObjAbrirCSV.abrircsv()  # abrindo e retornando conteúdo!
 
-        while active:
-            try:
-                escolhaOp = str
+        if alert == 0:
+            print("Olá! Seja bem vindo ao PYTHON FOR NOOBS!")
+            print("Sendo esta a sua primeira entrada")
+            print("você precisará criar um profile: -> ")
+            alert = ObjCriarProfile.criarprofile(alert)
+            print("GERANDO SCOREFILE!")
+            novoCod = "#"+str(alert + 1)
+            novoScore = 0
+            nomeArquivoSCORE = "score_"+novoCod+".csv"
+            novoPath = "PYTHON\projetos\PFNoobs\Opening\Data\score_"+novoCod+".csv"
+            with open(novoPath, 'w+', newline='') as csvfile:
+                filewriter = writer(csvfile, quotechar='|', quoting=QUOTE_MINIMAL)
+                filewriter.writerow(['SCORE'])
+                filewriter.writerow([novoScore])
+            csvfile.close()
 
-                print(" " * 15,"---- PYTHON FOR NOOBS ----\n")
-                print("\n", "-" * 76, "PFN-------DjPESSÔA -")             
-                print(f"User: {self.nome} <-> SCORE: {self.score}")
-                print("\nAbaixo estão todos os tipos de operadores:\n\n1 - Operadores Aritméticos\n2 - Operadores de Atribuição\n3 - Operadores de Comparação\n4 - Operadores Lógicos\n5 - Operadores de Identidade\n6 - Operadores de Associação\n7 - Sobre o programa\n8 - Sair\n","\n")
-                escolhaOp = str(input("Escolha os operadores que deseja estudar: "))
-                sair, self.score, codFinal = ObjEscolha.escolha(escolhaOp, self.score, self.cod)
-                # print("scoreFinal", self.score)
+        elif len(cod) != 0 or alert == 1:
+            entrarCriar = (str(input("Entrar - Criar - Sair -> "))).lower()
 
-                if sair == "ok":
-                    self.active = False
-                    # print("scoreInicio", self.score)
-                    return self.active, self.score, codFinal
-                elif sair == "no":
-                    self.active = True
-                else:
-                    raise ValueError("ERRO DESCONHECIDO!")
+        if entrarCriar in "criar":
+            print("Iniciando Criação: -> ")
+            alert = ObjCriarProfile.criarprofile(alert)
+            novoCod = "#"+str(alert + 1)
+            novoScore = 0
+            nomeArquivoSCORE = "score_"+novoCod+".csv"
+            novoPath = "PYTHON\projetos\PFNoobs\Opening\Data\score_"+novoCod+".csv"
+            with open(novoPath, 'w+', newline='') as csvfile:
+                filewriter = writer(csvfile, quotechar='|', quoting=QUOTE_MINIMAL)
+                filewriter.writerow(['SCORE'])
+                filewriter.writerow([novoScore])
+            csvfile.close()
 
-            except ValueError as e:
-                print("LOG: -> ", e)
+            raise ValueError("REINICIANDO!")
 
+        elif entrarCriar in "entrar":
+            confirmar, position = ObjEntrar.entrar(nome, senha)
+            if confirmar == 1:
+                print("Sua entrada foi permitida! Seja bem vindo!")
+                nomeUser = nome[position]
+                # print(nomeUser)
+                scoreUser = ObjAbrirSCORE.abrirScore(alert)
+                positionScore = len(scoreUser) - 1
+                scoreLido = int(scoreUser[positionScore])
+                # print(scoreLido)
+                codUser = cod[position]
+                # print(codUser)
+                active, scoreUserFinal, codUserFinal = ObjLayerMain.layermain(nomeUser, scoreLido, codUser, active)
+                # print(scoreUserFinal)
+                if active == False:
+                    active = ObjUpdateScore.updatescore(scoreUserFinal, codUserFinal, active)
+                    raise ValueError("SAINDO!")
+            elif confirmar == 0:
+                raise ValueError("LOADING -> ")
 
-ObjInicio = Inicio()
+        elif entrarCriar in "sair":
+            print("Seus dados estão salvos!")
+
+            active = False
+            raise ValueError("DESLIGANDO!")
+
+    except ValueError as e:
+        raise ValueError("LOG: ->", e)
+    finally:
+        continue
