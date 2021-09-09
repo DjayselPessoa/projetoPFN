defc03a6708cf1efbf75106949109687786c06fb
Djaysel Pessôa <djaysel@outlook.com>
2021-09-09 20:11:14 -0300

    Update LerScore.py

--- Core/LerScore.py
+++ Core/LerScore.py
@@ -1,17 +1,23 @@
 import csv
+from pathlib import Path
+from pathlib import PureWindowsPath
 
 
 class LerScore:
 
-    def lerscore(self, position):
+    def lerscore(self, position, pegarDirRaiz):
+        self.pegarDirRaiz = pegarDirRaiz
         self.position = position
         position = self.position
 
         scoreUser = []
         codPath = "#"+str(position - 1)
-        createNamePath = "PYTHON\projetos\PFNoobs\Opening\Data\score_"+codPath+".csv"
+        nomeArquivoSCORE = "score_"+codPath+".csv"
+        pathTratado = str(self.pegarDirRaiz)
+        home = Path(pathTratado)
+        sourcePath = Path(home, "Data", nomeArquivoSCORE)
 
-        for f in csv.DictReader(open(createNamePath), delimiter=','):
+        for f in csv.DictReader(open(sourcePath), delimiter=','):
             scoreUser.append(f["SCORE"])
 
         print(scoreUser)
