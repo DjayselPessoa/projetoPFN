6372f34ad13aaf98beaf2ddd740774f49b610799
Djaysel Pessôa <djaysel@outlook.com>
2021-09-09 20:07:21 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from Core.AbrirCSV import ObjAbrirSCORE
 from LayerMain import ObjLayerMain
 from Core.Entrar import ObjEntrar
@@ -6,30 +7,38 @@ from Data.CriarProfile import ObjCriarProfile
 from Data.UpdateScore import ObjUpdateScore
 from csv import writer
 from csv import QUOTE_MINIMAL
+import os
+
 
 active = True
 
 while active:
     try:
+        os.chdir(os.path.dirname(__file__))
+        pegarDirRaiz = str(os.getcwd())
+        # print("Ok1 ", pegarDirRaiz)
         alert = 0
         cod = []
         nome = []
         senha = []
         scorename = []
-
-        alert, cod, nome, senha, scorename = ObjAbrirCSV.abrircsv()  # abrindo e retornando conteúdo!
-
+        alert, cod, nome, senha, scorename = ObjAbrirCSV.abrircsv(pegarDirRaiz)  # abrindo e retornando conteúdo!
+        # print("alert 2", alert)
         if alert == 0:
             print("Olá! Seja bem vindo ao PYTHON FOR NOOBS!")
             print("Sendo esta a sua primeira entrada")
             print("você precisará criar um profile: -> ")
-            alert = ObjCriarProfile.criarprofile(alert)
+            alert = ObjCriarProfile.criarprofile(alert, pegarDirRaiz)
             print("GERANDO SCOREFILE!")
             novoCod = "#"+str(alert + 1)
             novoScore = 0
+
             nomeArquivoSCORE = "score_"+novoCod+".csv"
-            novoPath = "PYTHON\projetos\PFNoobs\Opening\Data\score_"+novoCod+".csv"
-            with open(novoPath, 'w+', newline='') as csvfile:
+            pathTratado = str(pegarDirRaiz)
+            home = Path(pathTratado)
+            sourcePath = Path(home, "Data", nomeArquivoSCORE)
+
+            with open(sourcePath, 'w+', newline='') as csvfile:
                 filewriter = writer(csvfile, quotechar='|', quoting=QUOTE_MINIMAL)
                 filewriter.writerow(['SCORE'])
                 filewriter.writerow([novoScore])
@@ -40,12 +49,16 @@ while active:
 
         if entrarCriar in "criar":
             print("Iniciando Criação: -> ")
-            alert = ObjCriarProfile.criarprofile(alert)
+            alert = ObjCriarProfile.criarprofile(alert, pegarDirRaiz)
             novoCod = "#"+str(alert + 1)
             novoScore = 0
+
             nomeArquivoSCORE = "score_"+novoCod+".csv"
-            novoPath = "PYTHON\projetos\PFNoobs\Opening\Data\score_"+novoCod+".csv"
-            with open(novoPath, 'w+', newline='') as csvfile:
+            pathTratado = str(pegarDirRaiz)
+            home = Path(pathTratado)
+            sourcePath = Path(home, "Data", nomeArquivoSCORE)
+
+            with open(sourcePath, 'w+', newline='') as csvfile:
                 filewriter = writer(csvfile, quotechar='|', quoting=QUOTE_MINIMAL)
                 filewriter.writerow(['SCORE'])
                 filewriter.writerow([novoScore])
@@ -59,7 +72,7 @@ while active:
                 print("Sua entrada foi permitida! Seja bem vindo!")
                 nomeUser = nome[position]
                 # print(nomeUser)
-                scoreUser = ObjAbrirSCORE.abrirScore(alert)
+                scoreUser = ObjAbrirSCORE.abrirScore(alert, pegarDirRaiz)
                 positionScore = len(scoreUser) - 1
                 scoreLido = int(scoreUser[positionScore])
                 # print(scoreLido)
@@ -68,7 +81,7 @@ while active:
                 active, scoreUserFinal, codUserFinal = ObjLayerMain.layermain(nomeUser, scoreLido, codUser, active)
                 # print(scoreUserFinal)
                 if active == False:
-                    active = ObjUpdateScore.updatescore(scoreUserFinal, codUserFinal, active)
+                    active = ObjUpdateScore.updatescore(scoreUserFinal, codUserFinal, active, pegarDirRaiz)
                     raise ValueError("SAINDO!")
             elif confirmar == 0:
                 raise ValueError("LOADING -> ")
