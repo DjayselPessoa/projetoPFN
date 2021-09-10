a4744b350dc92c7c2d46f698d7928f0bcc4cc5b9
Djaysel Pessôa <djaysel@outlook.com>
2021-09-10 05:26:42 -0300

    Update LayerMain.py

--- LayerMain.py
+++ LayerMain.py
@@ -14,10 +14,10 @@ class LayerMain:
         while active:
             try:
                 escolhaOp = str
-
-                print((" " * 15) + "---- PYTHON FOR NOOBS ----\n")
+                print("-" * 46)
+                print("\n" + ("-" * 20) + "---- PYTHON FOR NOOBS ----")
                 print("\n" + ("-" * 76) + "PFN-------DjPESSÔA -")             
-                print(f"User: {self.nome} <-> SCORE: {self.score}")
+                print(f"USER: {self.nome} <-> SCORE: {self.score}")
                 print("\nAbaixo estão todos os tipos de operadores:\n\n1 - Operadores Aritméticos\n2 - Operadores de Atribuição\n3 - Operadores de Comparação\n4 - Operadores Lógicos\n5 - Operadores de Identidade\n6 - Operadores de Associação\n7 - Sobre o programa\n8 - Sair\n","\n")
                 escolhaOp = str(input("Escolha os operadores que deseja estudar: "))
                 if not escolhaOp in "12345678":
