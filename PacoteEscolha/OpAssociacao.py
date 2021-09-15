e265a829a21e36c549f00330a83b627bbed19f47
Djaysel Pessôa <djaysel@outlook.com>
2021-09-14 21:14:52 -0300

    Update OpAssociacao.py

--- PacoteEscolha/OpAssociacao.py
+++ PacoteEscolha/OpAssociacao.py
@@ -7,12 +7,12 @@ class OpAssociacao:
         print("-" * 99)
         active = True
         while active:
-            print(f"score: {self.score}\nEscolha abaixo qual Operador de Associação você deseja aprender:\n","\n[1] - (Dentro in)\n[2] - Não em (not in)\n[3] - Voltar\n")
+            print(f"score: {self.score}\nEscolha abaixo qual Operador de Associação você deseja aprender:\n","\n[1] - Dentro (in)\n[2] - Não em (not in)\n[3] - Voltar\n")
             escolhaMenu = str(input("Escolha uma opção: "))
 
             try:
                 if escolhaMenu == "1":
-                    print("\nO operador de Associação [in] verifica se o valor da esquerda está contido dentro da sequência dos valores da direita. Ex.:\n")
+                    print("\nO operador de Associação (in) verifica se o valor da esquerda está contido dentro da sequência dos valores da direita. Ex.:\n")
                     print("\nnumero1 = 2, numero2 = 3, lista = [1,3,4]")
                     print("[A] - if num1 in lista:")
                     print("[B] - if num2 in lista:")
@@ -29,7 +29,7 @@ class OpAssociacao:
                         print("\nCaractere inválido! REINICIANDO!")
                         raise ValueError("-" * 99)
                 elif escolhaMenu == "2":
-                    print("\nO operador de Associação [not in] verifica se o valor da esquerda não está contido dentro da sequência dos valores da direita:\n")
+                    print("\nO operador de Associação (not in) verifica se o valor da esquerda não está contido dentro da sequência dos valores da direita:\n")
                     print("\nnumero1 = 2, numero2 = 3, lista = [1,3,4]")
                     print("[A] - if not num1 in lista:")
                     print("[B] - if not num2 in lista:")
