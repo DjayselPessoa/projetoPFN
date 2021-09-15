ef2e61b7b274e7ab547933080251a6995e14d417
Djaysel Pessôa <djaysel@outlook.com>
2021-09-14 21:08:03 -0300

    Update SobrePrograma.py

--- PacoteEscolha/SobrePrograma.py
+++ PacoteEscolha/SobrePrograma.py
@@ -7,13 +7,13 @@ class SobrePrograma:
         self.score = score
         try:
             escolha = ""
-            print("---------------------------------------------------------------------------------------------------")
+            print("-" * 98)
             print("\nOlá!\n","\nEspero que tenha gostado deste aplicativo PYTHON FOR NOOBS!\nEsta primeira versão é bem básica e utiliza somente o terminal para acesso.\n")
             print("Futuramente outras funcionalidades e provavelmente outra cara será dada à esse pequeno projeto.\nO objetivo deste programa é servir de consulta, mas de nenhum modo deve ser utilizado sozinho.\n")
             print("Sendo assim abaixo disponibilizo os links das fontes de onde essas informações sairam.\nPerceba também que com a evolução da linguagem algumas coisas podem deixar de funcionar como exposto aqui.\n")
             print("De todo modo agradeço sua paciência e espero que tenha gostado!\n")
             print("\nDjayselPessôa - programador/idealizador\n")
-            print("---------------------------------------------------------------------------------------------------")
+            print("-" * 98)
             print("\nhttps://stackoverflow.com/questions/22125114/how-to-insert-links-in-python\n")
             print("https://isolution.pro/pt/t/python/membership-operators-example/exemplo-de-operadores-de-associacao-python\n") 
             print("https://realpython.com/python-is-identity-vs-equality/\n")
@@ -22,13 +22,13 @@ class SobrePrograma:
             print("http://cristiancechinel.pro.br/my_files/algorithms/bookhtml/node39.html\n")
             print("https://isolution.pro/pt/t/python3/python-basic-operators/python-3-operadores-basicos\n")
             print("https://www.devmedia.com.br/operadores-no-python/40693\n")
-            print("---------------------------------------------------------------------------------------------------\n")
+            print("-" * 98)
 
             escolha = str(input("Para abrir todas as páginas acima no browser digite S. Para Voltar digite V:  "))
 
-            if escolha == "S" or escolha == "s":
+            if escolha in "Ss":
                 ObjAbrirBrowser.abrirBrowser()
-            elif escolha == "V" or escolha == "v":
+            elif escolha in "Vv":
                 return self.score
             else:
                 raise ValueError("Erro!")
