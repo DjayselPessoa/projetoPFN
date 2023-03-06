7556533d6491350c5c899294a08a53dfd4b2c52a
Djaysel Pessôa <djaysel@outlook.com>
2023-03-06 16:10:00 -0300

    ok5

--- Inicio.py
+++ Inicio.py
@@ -11,25 +11,42 @@ from csv import QUOTE_MINIMAL
 from time import sleep
 import os
 
+# -------------------------------------------------------------------------------------------------
+# Inicializando
+# -------------------------------------------------------------------------------------------------
 
 active = True
 
 while active:
     try:
+        # -------------------------------------------------------------------------------------------------
+        # Obtenção do caminho do diretório "atual do programa" até o arquivo início.py (main.py)
         os.chdir(os.path.dirname(__file__))
         pegarDirRaiz = str(os.getcwd())
+
+        # -------------------------------------------------------------------------------------------------
+        # Acima obtem-se o link do diretório atual do programa até este arquivo início.py
         # print("Ok1 ", pegarDirRaiz)
-        # ------------------------------------------------------------------------------------
-        # sizeTerminal = os.get_terminal_size()
-        # print("Tamanho do terminal: ", sizeTerminal)
-        # ------------------------------------------------------------------------------------
-        alert = 0
+        # -------------------------------------------------------------------------------------------------
+        sizeTerminal = os.get_terminal_size()
+        print("Tamanho do terminal: ", sizeTerminal)
+        # -------------------------------------------------------------------------------------------------
+        alert = 0  # variável do primeiro uso
+        # -------------------------------------------------------------------------------------------------
+        # listas de controle e dados abaixo:
+        # -------------------------------------------------------------------------------------------------
+       
         cod = []
         nome = []
         senha = []
         scorename = []
-        alert, cod, nome, senha, scorename = ObjAbrirCSV.abrircsv(pegarDirRaiz) # abrindo e retornando conteúdo!
-        # print("alert 2", alert)
+        # -------------------------------------------------------------------------------------------------
+        alert, cod, nome, senha, scorename = ObjAbrirCSV.abrircsv(pegarDirRaiz)  # abrindo e retornando conteúdo!
+        # -------------------------------------------------------------------------------------------------
+        # quando não tem nenhum usuário exibe mensagem de boas vindas
+        # print("alert 2: ", alert)
+
+        # -------------------------------------------------------------------------------------------------
         if alert == 0:
             print("Olá! Seja bem vindo ao PYTHON FOR NOOBS!")
             print("Já que esta é a sua primeira entrada")
@@ -51,6 +68,9 @@ while active:
                 filewriter.writerow([novoScore])
             csvfile.close()
 
+        # -------------------------------------------------------------------------------------------------
+        # Quando detectado ao menos um usuário o menu é mostrado
+        # -------------------------------------------------------------------------------------------------
         elif len(cod) != 0 or alert == 1:
             print("\nBem vindo ao PYTHON FOR NOOBS!\n\nUtilize as letras iniciais para acessar: ->\n")
             listaExibir = ["Entrar", "Criar", "Sair", "Rank", "[E]", "[C]", "[S]", "[R]"]
@@ -114,13 +134,12 @@ while active:
             raise ValueError("\nDESLIGANDO!")
 
         elif entrarCriar in "Rr":
-            ObjRankScore.rankscore(cod, pegarDirRaiz, nome)
+            # print(f"cod: {cod}")
+            ObjRankScore.rankscore(pegarDirRaiz, cod, nome, active)
             sleep(1)
             raise ValueError("\nLOADING - > ")
 
     except ValueError as e:
-        raise ValueError("\nLOG: ->", e)
+        print("\nLOG: ->", e)
     finally:
         continue
-        
-        
