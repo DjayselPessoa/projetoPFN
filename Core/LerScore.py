189b65799580a0d233a73f8c8c7a679e0d5737c8
Djaysel Pessôa <djaysel@outlook.com>
2023-03-06 16:01:47 -0300

    ok2

--- Core/LerScore.py
+++ Core/LerScore.py
@@ -5,27 +5,19 @@ from pathlib import Path
 class LerScore:
 
     def lerscore(self, nomeScore, pegarDirRaiz):
-        try:
-            self.pegarDirRaiz = pegarDirRaiz
-            self.nomeScore = nomeScore
-            # print(f"self.nomeScore: {self.nomeScore}")
-            scoreUser = []
-            nomeArquivoSCORE = str(self.nomeScore)
-            # print(f"nomeArquivoSCORE: {nomeArquivoSCORE}")
-            home = Path(self.pegarDirRaiz)
-            sourcePath = Path(home, "Data", nomeArquivoSCORE)
-            # print(sourcePath)
-            cont2 = 0
-            objetoScore = csv.DictReader(open(sourcePath), delimiter=',')
-            for f in objetoScore:
-                scoreUser.append(f["SCORE"])
-                print(f"cont2: {cont2} - lista {scoreUser[cont2]}")
-                cont2 += 1
-            mostrar = scoreUser[cont2]
-            print(f'scoreUser: {mostrar}')
-            return mostrar
-        except ValueError as e:
-            print("LOG ->", e)
+        self.pegarDirRaiz = pegarDirRaiz
+        self.nomeScore = nomeScore
+
+        scoreUser = []
+        nomeArquivoSCORE = self. nomeScore
+        home = Path(self.pegarDirRaiz)
+        sourcePath = Path(home, "Data", nomeArquivoSCORE)
+
+        for f in csv.DictReader(open(sourcePath), delimiter=','):
+            scoreUser.append(f["SCORE"])
+
+        # print(scoreUser)
+        return scoreUser[len(scoreUser) - 1]
 
 
 ObjLerScore = LerScore()
