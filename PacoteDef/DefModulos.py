8dae0c26b3441086cbf7678858d7565e6bd0aeec
Djaysel Pessôa <djaysel@outlook.com>
2023-03-08 02:03:46 -0300

    estruturando tela

---
+++ PacoteDef/DefModulos.py
@@ -0,0 +1,8 @@
+from tkinter import simpledialog, Tk
+
+# open dialog for input string
+# -----------------------------------------------------
+
+takerSTR = simpledialog.askstring("takerSTR","STORE:")
+
+print(takerSTR)
